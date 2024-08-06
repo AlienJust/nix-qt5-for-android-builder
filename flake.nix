162c8cf1abcfdd395dde375e19b00b6374ebc2de
Alexey Debelov <alienjustmail@gmail.com>
2024-08-06 10:53:26 +0500

    ABI

--- flake.nix
+++ flake.nix
@@ -1,12 +1,13 @@
-
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs @ { self, nixpkgs, ... }:
-
-  let
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    ...
+  }: let
     system = "x86_64-linux";
 
     pkgs = import nixpkgs {
@@ -18,15 +19,54 @@
     };
 
     qt5-for-android-builder = pkgs.callPackage ./default.nix {};
-  in
-  {
-
+  in {
     packages.x86_64-linux = {
       inherit qt5-for-android-builder;
     };
     packages.x86_64-linux.default = qt5-for-android-builder;
 
-  }
-  # // { inherit qt5-for-android-builder; }
-  ;
+    devShells.x86_64-linux = {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          #dotnet-sdk_6
+          #dotnet-sdk_7
+          #dotnet-sdk_8
+          qt5-for-android-builder
+
+          (with dotnetCorePackages;
+            combinePackages [
+              sdk_6_0
+              sdk_7_0
+              sdk_8_0
+            ])
+          omnisharp-roslyn
+          mono
+          msbuild
+          (pkgs.buildFHSEnv {
+            name = "avalonia-fhs-shell";
+            targetPkgs = pkgs:
+              with pkgs;
+                [
+                  udev
+                  alsa-lib
+                  fontconfig
+                  glew
+                ]
+                ++ (with pkgs.xorg; [
+                  # Avalonia UI
+                  libX11
+                  libICE
+                  libSM
+                  libXi
+                  libXcursor
+                  libXext
+                  libXrandr
+                ]);
+            #runScript = "zsh"; # same colors scheme of zsh confusing me
+          })
+        ];
+      };
+    };
+    # // { inherit qt5-for-android-builder; }
+  };
 }
